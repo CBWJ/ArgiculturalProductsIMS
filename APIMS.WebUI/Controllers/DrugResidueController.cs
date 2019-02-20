8362564061bf4a083baf2df59a99833cd93fdcf1
CBWJ <CBWJ@DESKTOP-N3E8BH9>
2019-02-20 23:25:31 +0800

    修改

---
+++ APIMS.WebUI/Controllers/DrugResidueController.cs
@@ -0,0 +1,18 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIMS.WebUI.Controllers
+{
+    public class DrugResidueController : BaseController
+    {
+        // GET: DrugResidue
+        public ActionResult Index()
+        {
+            SetModuleAuthority();
+            return View();
+        }
+    }
+}
